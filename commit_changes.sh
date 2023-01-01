35c7a0d55ab11f4f84f99b61cc1ee480f1fbe22e
inazak <keisuke.inazaki@gmail.com>
2023-01-01 23:19:13 +0900

    modified

--- commit_changes.sh
+++ commit_changes.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# using access token
+# git config --global credential.helper cache
+
 MODIFIED=$( cd $(dirname $0) ; git status | grep modified | wc -l )
 
 if [ $MODIFIED -ne 0 ] ; then
 
   cd $(dirname $0)
+  git add .
   git commit -m "modified"
+  git push origin main
 
 fi
 
--- current_ipaddr.sh
+++ current_ipaddr.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd $(dirname $0)
-curl inet-ip.info > current_ipaddr.txt
+LANG=C date        > current_ipaddr.txt
+curl inet-ip.info >> current_ipaddr.txt
 
